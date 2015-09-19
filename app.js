06389d87e4041a1f70a5fbe03a0e98f58b376837
Brien Pafford <bcpaff@gmail.com>
2015-09-19 16:17:35 -0500

    updated gitignore

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify 			= require('restify');
 var config 				= require('./config');
-var app 					= restify.createServer({name:'REST-api'});
+var app 					= restify.createServer({name:'hopList-1'});
 
 app.use(restify.fullResponse());
 app.use(restify.bodyParser());
